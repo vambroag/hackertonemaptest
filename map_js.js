fc6db50dcb6dd6b0858da81ccbae1dc942f853e7
vambroag <vambroag@gmail.com>
2023-08-13 15:12:14 +0900

    08/13maptest

--- map_js.js
+++ map_js.js
@@ -1,40 +1,76 @@
+let map;
+
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: 32.1582615544072, lng: 34.89155037133181 };
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: new google.maps.LatLng(37.559196356896, 126.97736606652),
+    zoom: 14,
+    mapId: '31889289191ab1d3'
+  });
 
-    // The map, centered at Uluru
-    /*const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 11,
-      center: uluru,
-      disableDefaultUI: true,
-    });*/
+  const iconBase =
+    "../img/mapmarkerimg/";
+  const icons = {
+    meat: {
+      icon: iconBase + "축산.png",
+    },
+    fish: {
+      icon: iconBase + "수산.png",
+    },
+    restaurant: {
+      icon: iconBase + "음식.png",
+    },
+  };
+  const features = [
+    {
+      position: new google.maps.LatLng(37.559349, 126.9773777),
+      type: "meat",
+    },
+    {
+      position: new google.maps.LatLng(37.559279, 126.977434),
+      type: "meat",
+    },
+    {
+      position: new google.maps.LatLng(37.5592515, 126.9773701),
+      type: "meat",
+    },
+    {
+      position: new google.maps.LatLng(37.5592209, 126.9774115),
+      type: "meat",
+    },
+    {
+      position: new google.maps.LatLng(37.5590489, 126.9777508),
+      type: "meat",
+    },
+    {
+      position: new google.maps.LatLng(37.559114, 126.9774099),
+      type: "fish",
+    },
+    {
+      position: new google.maps.LatLng(37.5590981, 126.9774544),
+      type: "fish",
+    },
+    {
+      position: new google.maps.LatLng(37.5590159, 126.9775489),
+      type: "fish",
+    },
+    {
+      position: new google.maps.LatLng(37.5592445, 126.977774),
+      type: "fish",
+    },
+    {
+      position: new google.maps.LatLng(37.5591502, 126.9775453),
+      type: "restaurant",
+    },
+  ];
 
-    const map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 37.515, lng: 127.05 },
-        zoom: 14,
-        mapId: '31889289191ab1d3'
-    });
-
-    const rectangle = new google.maps.Rectangle({
-        strokeColor: "#FF0000",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#FF0000",
-        fillOpacity: 0.35,
-        map,
-        bounds: {
-            north: uluru.lat - 0.005,
-            south: uluru.lat + 0.005,
-            west: uluru.lng - 0.005,
-            east: uluru.lng + 0.005,
-        },
-    });
-
-    // The marker, positioned at Uluru
-    /*const marker = new google.maps.Marker({
-      position: uluru,
+  // Create markers.
+  for (let i = 0; i < features.length; i++) {
+    const marker = new google.maps.Marker({
+      position: features[i].position,
+      icon: icons[features[i].type].icon,
       map: map,
-    });*/
+    });
+  }
 }
 
 window.initMap = initMap;
